9ef306991836cec8fbae7a3034cb4dfb2d2d30ab
mult1fractal <stormrider935@googlemail.com>
2021-09-07 11:36:41 +0200

    changed names of main script back to default phage_rework>phage.nf

--- bin/contig_by_tool_count.sh
+++ bin/contig_by_tool_count.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 
 # combine prediction results in one file with toolnames
 for x in *.tsv; do
@@ -8,21 +8,25 @@ done
 awk '{print $0"\t"FILENAME}' *.txt >  tmp_result.tsv
 awk '{ gsub(/.txt/,"", $3); print }' OFS='\t' tmp_result.tsv > tmp_results2.tsv
 rm *.txt
-
 sed -e '1i\contig_name\tp-value\ttoolname' tmp_results2.tsv > all_overview.tsv
-
-
 
 ## which tools were used?
 cut -f3 tmp_results2.tsv | sort -u > tools_used_for_phage_prediction.txt
 
 ## contig was found by how many tools
+## creates X_tools_agree_files with the contigs in it
+## and a overview file abount toolagreements per contig
+## does not take p value into consideration
 cut -f1 tmp_results2.tsv | sort -u > contig_list.txt
-grep -f contig_list.txt tmp_results2.tsv ## pigaro baut hier noch die outputfile falsch
-inputfile='contig2_list.txt'
+inputfile='contig_list.txt'
 while read line; do
-    toolcount=$(grep $line tmp_results2.tsv | cut -f3 | sort -u | wc -l)
-    echo "$line    $toolcount"
-done < $inputfile
+    toolcount=$(tail -n+2 all_overview.tsv | grep $line | cut -f3 | sort -u | wc -l)
+    ctg=$(tail -n+2 all_overview.tsv | grep $line | cut -f1 | sort -u)
+    touch tmp_tool_agreement_test/"$toolcount"_tools_agree.txt | echo "$ctg" >> tmp_tool_agreement_test/"$toolcount"_tools_agree.txt
+    echo "$line $toolcount"
+done < $inputfile > toolagreement_per_contig.tsv
+
+
+
 
 
